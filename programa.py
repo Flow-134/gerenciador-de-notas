19b951ef00f69544a8a2b2ce32f5313eebef85d7
Flow-134 <carlos.barroso20@aluno.ce.gov.br>
2025-10-24 10:21:24 -0300

    Add files via upload

--- programa.py
+++ programa.py
@@ -1,13 +1,25 @@
 from funçoes import *
+ListaNotas = []    
+ListaAlunos = []
 while True:
     print("Cadastrar aluno e notas - 1")
     print("Exibir Relatorio - 2")
     print("Sair do Programa - 0")
     op = input("Escolha uma opção: ")
-    if op == "1":
-        ListaNotas = []    
+    if op == "0":
+        break
+    elif op == "1":
         Aluno = input("Digite o nome do aluno")
         for i in range(3):
             ListaNotas.append(float(input("Digite uma nota: ")))
-            notadict = {Aluno : [ListaNotas]}
+        mediaAl= calcular_media(ListaNotas)
+        status = verificacao(mediaAl)
+        dadosDoAluno = [ListaNotas,mediaAl,status]
+        dictAluno = {
+            Aluno: dadosDoAluno 
+        }
+        ListaAlunos.append(dictAluno)
+    elif op == "2":
+        print(ListaAlunos)
             
+        
